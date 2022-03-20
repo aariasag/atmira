b1d4e4c2989a4c0312b66f030e8ca312fd881760
aariasag <alfonsoariasaguilera@gmail.com>
2022-03-20 20:03:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import plotly.express as px
 import datetime
 from wordcloud import WordCloud
+from PIL import Image
 
 #Set local path
 path = os.path.abspath(os.getcwd())
@@ -27,7 +28,8 @@ df = data_read()
 
 
 #### MENU DEL DASHBOARD ####
-st.sidebar.markdown("# Atmira Dashboard")
+image = Image.open(path + '/data/atida-mifarma-logo.png')
+st.sidebar.image(image,use_column_width=True)
 
 start_date = st.sidebar.date_input('Start date', value=datetime.date(2017,1,1), min_value=datetime.date(2017,1,1), max_value=datetime.date(2018,12,30))
 end_date = st.sidebar.date_input('End date', value=datetime.date(2018,12,30), min_value = datetime.date(2017,1,1), max_value=datetime.date(2018,12,30))
@@ -35,64 +37,24 @@ end_date = st.sidebar.date_input('End date', value=datetime.date(2018,12,30), mi
 category_list = list(df['analytic_category'].unique())
 selected_category = st.sidebar.multiselect('Choose a category', category_list,category_list[:])
 
-#### VENTAS POR CATEGORIA EN UN PERIODO ###
-latest_data = df[(df['date']<=end_date) & (df['date']>=start_date)]
+### TOP 10 MOST SOLD PRODUCTS
 
-grouped_data = (
-    latest_data.dropna(subset=['analytic_category']).groupby('analytic_category')['sales','benefit'].sum()
-               .sort_values('sales').reset_index()
-)
+max_word = st.sidebar.slider("Max words to display", 10, 100, 10 )
 
-filtered_data = grouped_data[grouped_data['analytic_category'].isin(selected_category)]
+latest_data = df[(df['date']<=end_date) & (df['date']>=start_date) & df['analytic_category'].isin(selected_category)]
+first_word = latest_data['name'].str.split(expand=True)[0].to_frame(name='name')
+d = dict(latest_data['name'].str.split(expand=True)[0].value_counts().to_frame().reset_index().values)
 
-fig  = px.bar(filtered_data.iloc[:],
-              x=["sales","benefit"],
-              y="analytic_category",
-              barmode = 'group',
-              orientation='h')
+st.write("""## Most repeated word in products""")
 
-st.write("""## Sales by Category""")
+wc = WordCloud(background_color="white",random_state=max_word,max_words=max_word)
+wc.generate_from_frequencies(frequencies=d)
+fig = px.imshow(wc)
+fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
 st.plotly_chart(fig)
 
-#### SCATTER PLOT DEL PRECIO DE UN PRODUCTO Y SU COSTE BASE PARA LA DETECCIÓN DE ANOMALÍAS ###
-
-grouped_data = (
-    latest_data.dropna(subset=['name', 'base_cost', 'price']).groupby(['analytic_category', 'name'])[
-        'price', 'base_cost','benefit'].mean().reset_index()
-
-)
-
-filtered_data = grouped_data[grouped_data['analytic_category'].isin(selected_category)]
-
-#Eliminamos los productos con precio o coste base cero
-filtered_data = filtered_data[(filtered_data.price>0) & (filtered_data.base_cost >0) & (filtered_data.benefit>0)]
-
-#Usamos escala logarítmica en x e y para una mejor representación
-fig = px.scatter(filtered_data,
-           x='price',
-           y='base_cost',
-           size = 'benefit',
-           color = 'analytic_category',
-          # size_max=60,
-           log_x = True,
-           log_y = True,
-           hover_name='name')
-
-fig.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=1.02,
-    xanchor="right",
-    x=1
-))
-
-st.write("""## Mean Base Cost vs Mean Product Price""")
-st.write("""Bubble size is proportional to mean product benefit""")
-st.plotly_chart(fig)
 
 #### EVOLUCIÓN TEMPORAL DE LAS VENTAS
-
-
 
 filtered_data = latest_data[latest_data['analytic_category'].isin(selected_category)]
 
@@ -129,26 +91,60 @@ st.write("""## Sales evolution per date and hour""")
 fig=px.imshow(selected_data2D)
 st.plotly_chart(fig)
 
-### TOP 10 MOST SOLD PRODUCTS
+#### SCATTER PLOT DEL PRECIO DE UN PRODUCTO Y SU COSTE BASE PARA LA DETECCIÓN DE ANOMALÍAS ###
 
-max_products = st.sidebar.slider("Productos más vendidos", 0, 100, 10 )
-
-latest_data = df[(df['date'] <= end_date) & (df['date'] >= start_date)]
-selected_data = (
-    latest_data.dropna(subset=['name']).groupby(['name'])['qty_ordered'].sum().reset_index()
+grouped_data = (
+    latest_data.dropna(subset=['name', 'base_cost', 'price']).groupby(['analytic_category', 'name'])[
+        'price', 'base_cost','benefit'].mean().reset_index()
 
 )
 
-st.write("""## TOP """ + str(max_products) + """ MOST SOLD PRODUCTS""")
+filtered_data = grouped_data[grouped_data['analytic_category'].isin(selected_category)]
 
-most_sold_products = selected_data.sort_values(by='qty_ordered',ascending=False).head(max_products).reset_index()
-most_sold_products = most_sold_products[['name','qty_ordered']]
-most_sold_products['qty_ordered'] = most_sold_products['qty_ordered'].astype(int)
+#Eliminamos los productos con precio o coste base cero
+filtered_data = filtered_data[(filtered_data.price>0) & (filtered_data.base_cost >0) & (filtered_data.benefit>0)]
 
-d = dict(most_sold_products.values)
+#Usamos escala logarítmica en x e y para una mejor representación
+fig = px.scatter(filtered_data,
+           template='simple_white',
+           x='price',
+           y='base_cost',
+           size = 'benefit',
+           color = 'analytic_category',
+           log_x = True,
+           log_y = True,
+           hover_name='name')
 
-wc = WordCloud(background_color="white",random_state=40)
-wc.generate_from_frequencies(frequencies=d)
-fig = px.imshow(wc)
-fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+))
+
+st.write("""## Mean Base Cost vs Mean Product Price""")
+st.write("""Bubble size is proportional to mean product benefit""")
 st.plotly_chart(fig)
+
+
+
+#### VENTAS POR CATEGORIA EN UN PERIODO ###
+latest_data = df[(df['date']<=end_date) & (df['date']>=start_date)]
+
+grouped_data = (
+    latest_data.dropna(subset=['analytic_category']).groupby('analytic_category')['sales','benefit'].sum()
+               .sort_values('sales').reset_index()
+)
+
+filtered_data = grouped_data[grouped_data['analytic_category'].isin(selected_category)]
+
+fig  = px.bar(filtered_data.iloc[:],
+              x=["sales","benefit"],
+              y="analytic_category",
+              barmode = 'group',
+              orientation='h')
+
+st.write("""## Sales by Category""")
+st.plotly_chart(fig)
+
